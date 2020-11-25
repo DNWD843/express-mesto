1b3fbdeaab10af923517aa3eb6fe5544f53617f7
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2020-11-25 20:53:27 +0300

    port 3000

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const handleErrors = require('./middlewares/handleErrors');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 /**
  * @module app
