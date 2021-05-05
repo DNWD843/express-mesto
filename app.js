d03c289c059fbcc9bd430111d8ca98292407f7b7
Дмитрий Неклюдов <dneklyudov@MacBook-Air-Dmitrij.local>
2021-05-05 20:12:06 +0300

    cors 05.05.21

--- app.js
+++ app.js
@@ -21,14 +21,8 @@ const app = express();
 //   origin: 'https://instamesto.nomoredomains.icu/',
 // };
 // app.use(cors(corsOptions));
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://instamesto.nomoredomains.icu/');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+app.use(cors());
 
-  next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
